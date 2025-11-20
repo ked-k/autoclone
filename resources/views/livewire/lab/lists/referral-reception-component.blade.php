ecfb8356c254cbea2082e463fc847087b02ba818
Ked <61323721+ked-k@users.noreply.github.com>
2025-11-21 00:06:21 +0300

    Update referral-reception-component.blade.php

--- resources/views/livewire/lab/lists/referral-reception-component.blade.php
+++ resources/views/livewire/lab/lists/referral-reception-component.blade.php
@@ -169,7 +169,7 @@
                                 <span class="badge bg-info">{{ $sample['status'] }}</span>
                         </td>
                         <td>
-                            @if(in_array($sample['status'] ,['Pending','Delivered','Rejected']))
+                            @if(in_array($sample['status'] ,['Pending','Delivered','Rejected','Received']))
                             <button wire:click="openAccessionModal({{ $index }})"
                                     class="btn btn-sm btn-primary"
                                     >
